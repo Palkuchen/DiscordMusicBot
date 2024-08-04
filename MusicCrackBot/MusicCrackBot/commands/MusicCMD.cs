4947cb40fd49b9b7f31e43970c914129127b76b9
Palkuchen <palkuchen@gmail.com>
2024-08-04 11:49:30 +0200

    added Skip, Volume & help Commands

--- MusicCrackBot/MusicCrackBot/commands/MusicCMD.cs
+++ MusicCrackBot/MusicCrackBot/commands/MusicCMD.cs
@@ -20,8 +20,7 @@ namespace MusicCrackBot.commands
         public async Task PlayMusic(CommandContext ctx, [RemainingText] string query)
         {
             await checkPreExecution(ctx);
-            var userVC = ctx.Member.VoiceState.Channel;
-            playFirstTrack(query, userVC, ctx);
+            playFirstTrack(query, ctx);
         }
 
         [Command("pause")]
@@ -29,20 +28,10 @@ namespace MusicCrackBot.commands
         {
             //PRE-EXECUTION CHECKS
             await checkPreExecution(ctx);
+            var conn = await connect(ctx);
+            var musicTrack = conn.CurrentState.CurrentTrack;
 
-            var userVC = ctx.Member.VoiceState.Channel;
-            var lavalinkInstance = ctx.Client.GetLavalink();
-
-            var node = lavalinkInstance.ConnectedNodes.Values.First();
-            var conn = node.GetGuildConnection(ctx.Member.VoiceState.Guild);
-
-            if (conn == null)
-            {
-                await ctx.Channel.SendMessageAsync("Lavalink Failed to connect!!!");
-                return;
-            }
-
-            if (conn.CurrentState.CurrentTrack == null)
+            if (musicTrack == null)
             {
                 await ctx.Channel.SendMessageAsync("No tracks are playing!!!");
                 return;
@@ -50,13 +39,7 @@ namespace MusicCrackBot.commands
 
             await conn.PauseAsync();
 
-            var pausedEmbed = new DiscordEmbedBuilder()
-            {
-                Color = DiscordColor.Yellow,
-                Title = "Track Paused!!"
-            };
-
-            await ctx.Channel.SendMessageAsync(embed: pausedEmbed);
+            await ctx.Channel.SendMessageAsync(embed: getPauseEmblem(musicTrack));
         }
 
         [Command("resume")]
@@ -64,34 +47,37 @@ namespace MusicCrackBot.commands
         {
             //PRE-EXECUTION CHECKS
             await checkPreExecution(ctx);
+            var conn = await connect(ctx);
+            var musicTrack = conn.CurrentState.CurrentTrack;
 
-            var userVC = ctx.Member.VoiceState.Channel;
-            var lavalinkInstance = ctx.Client.GetLavalink();
-
-            var node = lavalinkInstance.ConnectedNodes.Values.First();
-            var conn = node.GetGuildConnection(ctx.Member.VoiceState.Guild);
-
-            if (conn == null)
-            {
-                await ctx.Channel.SendMessageAsync("Lavalink Failed to connect!!!");
-                return;
-            }
-
-            if (conn.CurrentState.CurrentTrack == null)
+            if (musicTrack == null)
             {
                 await ctx.Channel.SendMessageAsync("No tracks are playing!!!");
                 return;
             }
 
             await conn.ResumeAsync();
+            await ctx.Channel.SendMessageAsync(embed: getResumeEmblem(musicTrack));
+        }
 
-            var resumedEmbed = new DiscordEmbedBuilder()
+        [Command("skip")]
+        public async Task SkipMusic(CommandContext ctx)
+        {
+
+            //PRE-EXECUTION CHECKS
+            await checkPreExecution(ctx);
+            var conn = await connect(ctx);
+            var musicTrack = conn.CurrentState.CurrentTrack;
+
+            if (musicTrack == null)
             {
-                Color = DiscordColor.Green,
-                Title = "Resumed"
-            };
+                await ctx.Channel.SendMessageAsync("No tracks are playing!!!");
+                return;
+            }
 
-            await ctx.Channel.SendMessageAsync(embed: resumedEmbed);
+            await conn.StopAsync();      
+
+            await ctx.Channel.SendMessageAsync(embed: getSkipEmblem(musicTrack));
         }
 
         [Command("stop")]
@@ -100,8 +86,227 @@ namespace MusicCrackBot.commands
 
             //PRE-EXECUTION CHECKS
             await checkPreExecution(ctx);
+            var conn = await connect(ctx);
 
-            var userVC = ctx.Member.VoiceState.Channel;
+            if (conn.CurrentState.CurrentTrack == null)
+            {
+                await ctx.Channel.SendMessageAsync("No tracks are playing!!!");
+                return;
+            }
+
+            songs.Clear();
+            await conn.StopAsync();
+
+            await ctx.Channel.SendMessageAsync(embed: getStopEmblem());
+        }
+
+        [Command("leave")]
+        public async Task leaveChannel(CommandContext ctx)
+        {
+            //PRE-EXECUTION CHECKS
+            await checkPreExecution(ctx);
+            var conn = await connect(ctx);
+
+            await conn.StopAsync();
+            await conn.DisconnectAsync();
+
+            await ctx.Channel.SendMessageAsync(embed: getLeaveEmblem());
+        }
+
+        [Command("help")]
+        public async Task help(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync(embed: getHelpEmblem());
+        }
+
+        [Command("volume")]
+        public async Task setVolume(CommandContext ctx, [RemainingText] int volume)
+        {
+            var conn = await connect(ctx);
+            await conn.SetVolumeAsync(volume);
+            await ctx.Channel.SendMessageAsync(embed: getVolumeEmblem(volume));
+        }
+        public DiscordEmbedBuilder getMusicEmblem(LavalinkTrack musicTrack)
+        {
+            string musicDescription = $"Now Playing: {musicTrack.Title} \n" +
+                                      $"Author: {musicTrack.Author} \n" +
+                                      $"length: {musicTrack.Length} \n" +
+                                      $"URL: {musicTrack.Uri}";
+
+            var nowPlayingEmbed = new DiscordEmbedBuilder()
+            {
+                Color = DiscordColor.Purple,
+                Title = $"Successfully playing music",
+                Description = musicDescription
+            };
+
+            return nowPlayingEmbed;
+        }
+        public DiscordEmbedBuilder getQueueEmblem(LavalinkTrack musicTrack)
+        {
+            string musicDescription = $"Added: {musicTrack.Title} to queue \n" +
+                                      $"Author: {musicTrack.Author} \n" +
+                                      $"length: {musicTrack.Length} \n" +
+                                      $"URL: {musicTrack.Uri}";
+
+            var nowPlayingEmbed = new DiscordEmbedBuilder()
+            {
+                Color = DiscordColor.Blue,
+                Title = $"Successfully added Song {musicTrack.Title} to queue",
+                Description = musicDescription
+            };
+
+            return nowPlayingEmbed;
+        }
+        public DiscordEmbedBuilder getPauseEmblem(LavalinkTrack musicTrack)
+        {
+            string musicDescription = $"Track: {musicTrack.Title}\n" +
+                                      $"length to go: {musicTrack.Length - musicTrack.Position}";
+
+            var pausedEmbed = new DiscordEmbedBuilder()
+            {
+                Color = DiscordColor.Yellow,
+                Title = $"Track Paused",
+                Description = musicDescription
+            };
+
+            return pausedEmbed;
+        }
+        public DiscordEmbedBuilder getStopEmblem()
+        {
+
+            var stopEmbed = new DiscordEmbedBuilder()
+            {
+                Color = DiscordColor.Orange,
+                Title = "Stopped playing tracks",
+                Description = "Successfully stopped playing, leaving with !leave"
+            };
+
+            return stopEmbed;
+        }
+        public DiscordEmbedBuilder getVolumeEmblem(int volume)
+        {
+
+            var stopEmbed = new DiscordEmbedBuilder()
+            {
+                Color = DiscordColor.Green,
+                Title = "Sucsesfully changed Volume",
+                Description = $"Changed Volume to {volume}"
+            };
+
+            return stopEmbed;
+        }
+        public DiscordEmbedBuilder getLeaveEmblem()
+        {
+
+            var stopEmbed = new DiscordEmbedBuilder()
+            {
+                Color = DiscordColor.Red,
+                Title = "Disconnected from VC",
+                Description = "Successfully stopped the track and left the channeö"
+            };
+
+            return stopEmbed;
+        }
+        public DiscordEmbedBuilder getSkipEmblem(LavalinkTrack musicTrack)
+        {
+
+            var stopEmbed = new DiscordEmbedBuilder()
+            {
+                Color = DiscordColor.Green,
+                Title = $"Skipped {musicTrack.Title}",
+                Description = "Successfully skipped the track going on in Queue"
+            };
+
+            return stopEmbed;
+        }
+        public DiscordEmbedBuilder getResumeEmblem(LavalinkTrack musicTrack)
+        {
+            string musicDescription = $"Track: {musicTrack.Title}\n" +
+                                      $"length to go: {musicTrack.Length - musicTrack.Position}";
+
+            var pausedEmbed = new DiscordEmbedBuilder()
+            {
+                Color = DiscordColor.Green,
+                Title = $"Track Paused",
+                Description = musicDescription
+            };
+
+            return pausedEmbed;
+        }
+        public DiscordEmbedBuilder getHelpEmblem()
+        {
+            string musicDescription = $"!play [Track Titel]: plays a Track\n" +
+                                      $"!skip: skipps the current Track\n" +
+                                      $"!leave: Disconnects from Channel\n" +
+                                      $"!resume: starts the Track after pausing\n" +
+                                      $"!Pause: pauses the Track";
+
+            var pausedEmbed = new DiscordEmbedBuilder()
+            {
+                Color = DiscordColor.HotPink,
+                Title = $"These Commands are avalebale",
+                Description = musicDescription
+            };
+
+            return pausedEmbed;
+        }
+
+
+        // Plays first track found in Grid
+        public async void playFirstTrack(String query, CommandContext ctx)
+        {
+            var lavalinkInstance = ctx.Client.GetLavalink();
+            var node = lavalinkInstance.ConnectedNodes.Values.First();
+            await node.ConnectAsync(ctx.Member.VoiceState.Channel);
+
+            textChannel = ctx.Channel;
+
+            var conn = await connect(ctx);
+
+            var searchQuery = await node.Rest.GetTracksAsync(query);
+            if (searchQuery.LoadResultType == LavalinkLoadResultType.NoMatches || searchQuery.LoadResultType == LavalinkLoadResultType.LoadFailed)
+            {
+                await ctx.Channel.SendMessageAsync($"Failed to find music with query: {query}");
+                return;
+            }
+
+            var musicTrack = searchQuery.Tracks.First();
+
+            if (conn.CurrentState.CurrentTrack != null)
+            {
+                // Adding to queue
+                songs.Add(musicTrack);
+                await ctx.Channel.SendMessageAsync(embed: getQueueEmblem(musicTrack));
+                return;
+            }
+
+            await conn.SetVolumeAsync(5);
+            await conn.PlayAsync(musicTrack);
+            conn.PlaybackFinished += Conn_PlaybackFinished;
+
+            await ctx.Channel.SendMessageAsync(embed: getMusicEmblem(musicTrack));
+        }
+
+        // Could be changed to just playTrack
+        public async void playQueuedTrack(LavalinkGuildConnection conn, LavalinkTrack musicTrack)
+        {
+            if (conn == null)
+            {
+                await textChannel.SendMessageAsync("Lavalink Failed to connect!!!");
+                return;
+            }
+
+            await conn.PlayAsync(musicTrack);
+            conn.PlaybackFinished += Conn_PlaybackFinished;
+
+            await textChannel.SendMessageAsync(embed: getMusicEmblem(musicTrack));
+        }
+
+        public async Task<LavalinkGuildConnection> connect(CommandContext ctx)
+        {
+            // Hanels the connection to lavalink
+
             var lavalinkInstance = ctx.Client.GetLavalink();
 
             var node = lavalinkInstance.ConnectedNodes.Values.First();
@@ -110,28 +315,11 @@ namespace MusicCrackBot.commands
             if (conn == null)
             {
                 await ctx.Channel.SendMessageAsync("Lavalink Failed to connect!!!");
-                return;
+                return null;
             }
 
-            if (conn.CurrentState.CurrentTrack == null)
-            {
-                await ctx.Channel.SendMessageAsync("No tracks are playing!!!");
-                return;
-            }
-
-            await conn.StopAsync();
-            await conn.DisconnectAsync();
-
-            var stopEmbed = new DiscordEmbedBuilder()
-            {
-                Color = DiscordColor.Red,
-                Title = "Stopped the Track",
-                Description = "Successfully disconnected from the VC"
-            };
-
-            await ctx.Channel.SendMessageAsync(embed: stopEmbed);
+            return conn;
         }
-
         private Task Conn_PlaybackFinished(LavalinkGuildConnection sender, DSharpPlus.Lavalink.EventArgs.TrackFinishEventArgs args)
         {
             if (songs.Count > 0)
@@ -146,7 +334,6 @@ namespace MusicCrackBot.commands
                 return sender.Channel.SendMessageAsync($"Nothing left in queue");
             }
         }
-
         public async Task checkPreExecution(CommandContext ctx)
         {
             if (ctx.Member == null)
@@ -173,93 +360,6 @@ namespace MusicCrackBot.commands
             }
 
             return;
-        }
-
-        public DiscordEmbedBuilder getMusicEmblem(LavalinkTrack musicTrack, DiscordChannel userVC)
-        {
-            string musicDescription = $"Now Playing: {musicTrack.Title} \n" +
-                                      $"Author: {musicTrack.Author} \n" +
-                                      $"length: {musicTrack.Length} \n" +
-                                      $"URL: {musicTrack.Uri}";
-
-            var nowPlayingEmbed = new DiscordEmbedBuilder()
-            {
-                Color = DiscordColor.Purple,
-                Title = $"Successfully joined channel {userVC.Name} and playing music",
-                Description = musicDescription
-            };
-
-            return nowPlayingEmbed;
-        }
-        public DiscordEmbedBuilder getQueueEmblem(LavalinkTrack musicTrack, DiscordChannel userVC)
-        {
-            string musicDescription = $"Added: {musicTrack.Title} to queue \n" +
-                                      $"Author: {musicTrack.Author} \n" +
-                                      $"length: {musicTrack.Length} \n" +
-                                      $"URL: {musicTrack.Uri}";
-
-            var nowPlayingEmbed = new DiscordEmbedBuilder()
-            {
-                Color = DiscordColor.Blue,
-                Title = $"Successfully added Song {musicTrack.Title} to queue",
-                Description = musicDescription
-            };
-
-            return nowPlayingEmbed;
-        }
-
-        // Plays first track found in Grid
-        public async void playFirstTrack(String query, DiscordChannel userVC, CommandContext ctx)
-        {
-            var lavalinkInstance = ctx.Client.GetLavalink();
-            var node = lavalinkInstance.ConnectedNodes.Values.First();
-            await node.ConnectAsync(userVC);
-
-            textChannel = ctx.Channel;
-
-            var conn = node.GetGuildConnection(ctx.Member.VoiceState.Guild);
-            if (conn == null)
-            {
-                await textChannel.SendMessageAsync("Lavalink Failed to connect!!!");
-                return;
-            }
-
-            var searchQuery = await node.Rest.GetTracksAsync(query);
-            if (searchQuery.LoadResultType == LavalinkLoadResultType.NoMatches || searchQuery.LoadResultType == LavalinkLoadResultType.LoadFailed)
-            {
-                await ctx.Channel.SendMessageAsync($"Failed to find music with query: {query}");
-                return;
-            }
-
-            var musicTrack = searchQuery.Tracks.First();
-
-            if (conn.CurrentState.CurrentTrack != null)
-            {
-                // Adding to queue
-                songs.Add(musicTrack);
-                await ctx.Channel.SendMessageAsync(embed: getQueueEmblem(musicTrack, userVC));
-                return;
-            }
-
-            await conn.PlayAsync(musicTrack);
-            conn.PlaybackFinished += Conn_PlaybackFinished;
-
-            await ctx.Channel.SendMessageAsync(embed: getMusicEmblem(musicTrack, userVC));
-        }
-
-        // Could be changed to just playTrack
-        public async void playQueuedTrack(LavalinkGuildConnection conn, LavalinkTrack musicTrack)
-        {
-            if (conn == null)
-            {
-                await textChannel.SendMessageAsync("Lavalink Failed to connect!!!");
-                return;
-            }
-
-            await conn.PlayAsync(musicTrack);
-            conn.PlaybackFinished += Conn_PlaybackFinished;
-
-            await textChannel.SendMessageAsync(embed: getMusicEmblem(musicTrack, textChannel));
         }
     }
 }
